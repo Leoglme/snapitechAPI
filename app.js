60371a2e9dc493e1c690612b4418451a512c9845
Guillaume <leo.guillaume@epitech.eu>
2021-05-30 21:25:55 +0200

    fix

--- app.js
+++ app.js
@@ -59,7 +59,12 @@ app.post('/snap',upload.single('fileData'), (req, res,next) => {
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use( bodyParser.json({limit: '50mb'}) );
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit:50000
+}));
 app.post('/upload/snap', (req, res) => {
     fs.writeFile('./out.png', req.body.imgsource, 'base64', (err) => {
         if (err) throw err
