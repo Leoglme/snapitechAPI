b4df2f1110e5f765b1044a7d2242a0312224ef5e
Guillaume <leo.guillaume@epitech.eu>
2021-05-31 01:21:34 +0200

    fix

--- app.js
+++ app.js
@@ -21,42 +21,6 @@ app.get("/", (req, res) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-let storage;
-
-if (process.env.NODE_ENV === 'production') {
-    storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, path.resolve(__dirname, 'build'))
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname)
-        }
-    })
-} else {
-    storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, path.resolve(__dirname, 'uploads'))
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname)
-        }
-    })
-}
-
-
-const uploads = multer({ storage: storage });
-
-app.use(uploads.any());
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-    console.log(path.resolve(__dirname, 'build'));
-    console.log(express.static(path.resolve(__dirname, 'build')));
-} else {
-    app.use(express.static('./public'));
-}
-
-//if you need to download (after upload) files in cloudinary
 const cloudinary = require('cloudinary');
 cloudinary.config({
     cloud_name: 'dibodev',
