00fc6a0604428b34192e87bf16c5d0ba1b74aabd
Guillaume <leo.guillaume@epitech.eu>
2021-05-30 03:34:12 +0200

    fix

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const router = express.Router();
 const expressValidator = require('express-validator');
 const multer = require('multer')
+router.use(expressValidator())
 
 app.use(cors({origin: "*"}));
 app.use(bodyParser.json());
