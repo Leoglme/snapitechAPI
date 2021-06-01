ec0301123cc074b39446ebbc67252bc028893417
Guillaume <leo.guillaume@epitech.eu>
2021-06-01 15:44:32 +0200

    auto push heroku

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.post('/upload/imgur', (req, res, next) => {
         }
     }
 
-    request('https://api.imgur.com/3/image/g4Jyo7L', options, function (error, response, body) {
+    request('https://api.imgur.com/3/image', options, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
