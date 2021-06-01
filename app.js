e8d2a6af4f100ff441aca45acbbb979669ca3737
Guillaume <leo.guillaume@epitech.eu>
2021-06-01 15:21:29 +0200

    auto push

--- app.js
+++ app.js
@@ -84,11 +84,11 @@ app.post('/upload/imgur', (req, res, next) => {
 
     const options = {
         headers: {
-            Authorization: 'Client-ID dc6e79e2ed95c6b'
+            Authorization: apiKey
         }
     }
 
-    request('https://api.imgur.com/3/image/g4Jyo7L', options, function (error, response, body) {
+    request.post('https://api.imgur.com/3/image/g4Jyo7L', options, data, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
